7d280c9f51ff7005c0307b0effbf34824e929115
santanam23 <mirmir11.ms@gmail.com>
2022-04-05 11:41:03 -0500

    test passed

--- photo-port/src/components/Nav/__tests__/index.test.js
+++ photo-port/src/components/Nav/__tests__/index.test.js
@@ -29,9 +29,7 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
-
 })
--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,51 +1,55 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-const categories = [
-  {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
 function Nav() {
-  function categorySelected(name) {
-    console.log(`${name} clicked`)
-  }
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
-    <a href="/">
-      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className="mx-2">
-        <a href="#about">
-          About me
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
-      </li>
-      <li>
-        <span>Contact</span>
-      </li>
-      {categories.map((category) => (
-      <li
-        className="mx-1"
-        key={category.name}
-      >
-        <span onClick={categorySelected}>
-          {category.name}
-          </span>
-        </li>
-        ))}
-    </ul>
-  </nav>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => handleClick("About")}>
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
+          </li>
+          {
+            categories.map((category) => (
+              <li className="mx-1" key={category.name} >
+                <span onClick={() => { handleClick(category.name); }}>
+                 {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
     </header>
   );
 }
