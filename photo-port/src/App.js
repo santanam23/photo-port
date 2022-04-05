55c0778dc20c4a235fce684d462c3a5201470039
santanam23 <mirmir11.ms@gmail.com>
2022-04-05 10:34:12 -0500

    redoing components

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Nav from './components/Nav';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+        
+      </Nav>
       <main>
         <About></About>
       </main>
     </div>
   );
 }
+
 export default App;
