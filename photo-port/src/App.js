e9d62bba92ecde364fb1a3d8f7fc9a17bc8926aa
santanam23 <mirmir11.ms@gmail.com>
2022-04-05 20:48:10 -0500

    created gallery component

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         
       </Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
