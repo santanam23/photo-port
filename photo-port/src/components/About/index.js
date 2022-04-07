efdaf3b1873e236e97ce9a8a60f79bef321eb131
santanam23 <mirmir11.ms@gmail.com>
2022-04-07 09:22:41 -0500

    created ontact js

--- photo-port/src/components/About/index.js
+++ photo-port/src/components/About/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 import coverImage from "../../assets/cover/cover-image.jpg";
 
-
 function About() {
+
   return (
     <section className="my-5" >
       <h1 id="about">Who am I?</h1>
